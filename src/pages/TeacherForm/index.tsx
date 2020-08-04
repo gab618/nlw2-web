2809461a46abc9f7ffbc9d695e78e3fdcf326687
Gabriel <knuckles4x@gmail.com>
2020-08-04 00:10:35 -0300

    Header + form in TeacherList (mobile)

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import PageHeader from "../../components/PageHeader";
 
 export default function TeacherForm() {
   return (
-    <div>
-      <h1>TeacherForm</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
     </div>
   );
 }
